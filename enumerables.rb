baa3d31e958294c43187b6a6aa8ed2f114632fe9
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-06-25 07:46:38 -0300

    Create my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -8,6 +8,11 @@ module Enumerable
   end
 
   def my_each_with_index
+    i = 0
+    while i < self.length
+      yield self[i], i
+      i += 1
+    end
   end
 
   def my_select
@@ -44,11 +49,19 @@ module Enumerable
     self.my_inject { |total, item| total * item }
   end
 end
+
 # test my_each method
 puts #just to skip a line
 print "Test My Each"
 puts
 [1, 2, 3, 4, 5].my_each { | item | puts item * 2 }
+puts #just to skip a line
+
+# test my_each_with_index method
+puts #just to skip a line
+print "Test My Each With Index"
+puts
+[1, 2, 3, 4, 5].my_each_with_index { | item, index | puts "Element #{item} with index #{index}" }
 puts #just to skip a line
 
 print "Test My Count"
