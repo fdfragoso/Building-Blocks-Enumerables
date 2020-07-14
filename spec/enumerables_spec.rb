46471be4efd7fafaee57fa60bde9a7ea23866797
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-07-14 15:34:37 +0200

    Create my_each_with_index rspec

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,7 +2,7 @@ require_relative '../enumerables.rb'
 
 RSpec.describe Enumerable do
   include Enumerable
-  let(:arr) { [1, 2, 3] }
+  let(:arr) { [4, 5, 6] }
   let(:arr2) { [1, -2, 3] }
   let(:words) { %w[ant bear cat] }
   let(:words2) { %w[ant sheep dinousaur] }
@@ -23,12 +23,26 @@ RSpec.describe Enumerable do
       expect(arr.my_each { |x| puts x }).to eql(arr.each { |x| puts x })
     end
 
-    it 'returns an enumarator if no block is given' do
+    it 'returns an enumerator if no block is given' do
       expect(arr.my_each).to be_a(Enumerator)
     end
 
     it 'should throw an argument error if passed an argument' do
-      expect { arr.my_each('whatever') }.to raise_error(ArgumentError)
+      expect { arr.my_each('test') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'returns exaclty the same as the original function when passed a block' do
+      expect(arr2.my_each { |x| puts x + 3 * 2 }).to eql(arr2.each { |x| puts x + 3 * 2 })
+    end
+
+    it 'returns an enumerator if no block is given' do
+      expect(arr2.my_each).to be_a(Enumerator)
+    end
+
+    it 'should throw an argument error if passed an argument' do
+      expect { arr2.my_each('test') }.to raise_error(ArgumentError)
     end
   end
 end
