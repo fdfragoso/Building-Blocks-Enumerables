469d5c020fb8f951eb69d2d6689c250b42c4e618
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-07-14 17:01:01 +0200

    Create my_map and my_inject rspec

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Enumerable do
   let(:arr) { [1, 2, 3] }
   let(:arr2) { [1, -2, 3] }
   let(:words) { %w[ant bear cat] }
-  let(:words2) { %w[ant sheep dinousaur] }
+  let(:words2) { %w[ant sheep dinosaur] }
   let(:mixed) { [nil, true, 99] }
   let(:longest) do
     proc do |memo, word|
@@ -99,21 +99,56 @@ RSpec.describe Enumerable do
 
      it 'returns the length of the block' do
         expect(words2.my_any? { |num| num.length <=1 } ).to eql(false)
-     end
+    end
   end
 
   describe '#my_none?' do
-  it 'returns true if certifies the condition ' do
-    expect(arr.my_none?).to eql(false)
-  end
+    it 'returns true if certifies the condition ' do
+      expect(arr.my_none?).to eql(false)
+    end
 
-  it 'returns the length of the block' do
-    expect(words.my_none? { |num| num.length >= 5} ).to eql(true)
-  end
+    it 'returns the length of the block' do
+      expect(words.my_none? { |num| num.length >= 5} ).to eql(true)
+    end
 
-   it 'returns the length of the block' do
+    it 'returns the length of the block' do
       expect(words2.my_none? { |num| num.length <=20 } ).to eql(false)
-   end
-end
+    end
+  end
 
+  describe '#my_inject' do
+    it 'accepts a symbol that references a block as an argument' do
+      expect(arr.my_inject(:+)).to eq(6)
+    end
+  
+    it 'accepts a block' do
+      expect((5..10).my_inject(1, :*)).to eq(151_200)
+    end
+
+    it 'accepts an argument as an initiator value well a block' do
+      expect(arr2.my_inject(1) { |product, n| product * n}).to eq(-6)
+    end
+
+    it 'accepts an argument as an initiator value well a block' do
+      expect(words2.my_inject(&longest)).to eq('dinosaur')
+    end
+  end
+
+  describe '#my_map' do
+    it 'accepts a symbol that references a block as an argument' do
+      expect(arr.my_map(&:to_s)).to eq(%w[1 2 3])
+    end
+  
+    it 'accepts a block and returns it as array' do
+      expect(arr.my_map { |i| i * i }).to eq([1, 4, 9])
+    end
+  
+    it 'returns an item dictated by the block passed to it for every item in the array' do
+      expect(arr.my_map { 'cat' }).to eql(Array.new(3, 'cat'))
+    end
+
+    it 'if both proc argument and block given, the proc will be used' do
+      expect(arr.my_map(identify) { |_item| 'block' }).to eql(Array.new(3, 'proc'))
+    end
+  end
 end
