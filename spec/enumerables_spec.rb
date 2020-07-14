0efe24be06b95de24b1e174c9929531459f96352
haraphat01 <haraphat@gmail.com>
2020-07-14 15:02:04 +0100

    my select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,7 +2,7 @@ require_relative '../enumerables.rb'
 
 RSpec.describe Enumerable do
   include Enumerable
-  let(:arr) { [4, 5, 6] }
+  let(:arr) { [1, 2, 3] }
   let(:arr2) { [1, -2, 3] }
   let(:words) { %w[ant bear cat] }
   let(:words2) { %w[ant sheep dinousaur] }
@@ -43,6 +43,20 @@ RSpec.describe Enumerable do
 
     it 'should throw an argument error if passed an argument' do
       expect { arr2.my_each('test') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns exaclty the same as the original function when passed a block' do
+      expect (arr.my_select  (&:even?) ).to eql([2])
+    end
+
+    it 'returns an enumerator if no block is given' do
+      expect(arr.my_select).to be_a(Enumerator)
+    end
+
+    it 'should throw an argument error if passed an argument' do
+      expect { arr.my_select('test') }.to raise_error(ArgumentError)
     end
   end
 end
