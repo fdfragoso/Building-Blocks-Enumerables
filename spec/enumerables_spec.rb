492cc5900cbf6c330a0ead04e27cc0a3d6bb8e3c
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-07-16 01:44:07 +0200

    Remove puts

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Enumerable do
 
   describe '#my_each' do
     it 'returns exaclty the same as the original function when passed a block' do
-      expect(arr.my_each { |x| puts x }).to eql(arr.each { |x| puts x })
+      expect(arr.my_each { |x| x }).to eql(arr.each { |x| x })
     end
 
     it 'returns an enumerator if no block is given' do
@@ -34,7 +34,7 @@ RSpec.describe Enumerable do
 
   describe '#my_each_with_index' do
     it 'returns exaclty the same as the original function when passed a block' do
-      expect(arr2.my_each { |x| puts x + 3 * 2 }).to eql(arr2.each { |x| puts x + 3 * 2 })
+      expect(arr2.my_each { |x| x + 3 * 2 }).to eql(arr2.each { |x| x + 3 * 2 })
     end
 
     it 'returns an enumerator if no block is given' do
